00cb045312823eb1a0165e7be9b0afe6eba15fa7
Feli <felinegri5@gmail.com>
2021-02-08 16:53:03 -0300

    1.0.1

--- ServerStatusBot/index.js
+++ ServerStatusBot/index.js
@@ -41,14 +41,23 @@ async function SendMessage(channel){
             }).catch((error) => {
                 console.log("Timeout or error on " + x.ServerIp + ":" + x.ServerPort)
             })
-
-            players += server.players.length;
-            maxplayers += server.maxplayers;
-            embed.addFields(
-                {
-                    name: config.EmbedInfo.ServerField1.replace("<servername>", server.name), value: config.EmbedInfo.ServerField2.replace("<ip>", server.connect).replace("<map>", server.map).replace("<players>", server.players.length).replace("<maxplayers>", server.maxplayers)
+            if(server != null){
+                players += server.players.length;
+                maxplayers += server.maxplayers;
+                embed.addFields(
+                    {
+                        name: config.EmbedInfo.ServerField1.replace("<servername>", server.name), value: config.EmbedInfo.ServerField2.replace("<ip>", server.connect).replace("<map>", server.map).replace("<players>", server.players.length).replace("<maxplayers>", server.maxplayers)
+                    }
+                )
+                if(config.ShowPlayers){
+                    for(var o = 0; o < server.players.length; o++){
+                        var player = server.players[o];
+                        embed.addFields({
+                            name: ":bust_in_silhouette: " + player.name, value: ":hourglass_flowing_sand: " + GetTime(player.time)
+                        })
+                    }
                 }
-            )
+            }
         }
     }
     finally{
@@ -74,14 +83,23 @@ async function UpdateEmbed (message){
             }).catch((error) => {
                 console.log("Timeout or error on " + x.ServerIp + ":" + x.ServerPort)
             })
-
-            players += server.players.length;
-            maxplayers += server.maxplayers;
-            embed.addFields(
-                {
-                    name: config.EmbedInfo.ServerField1.replace("<servername>", server.name), value: config.EmbedInfo.ServerField2.replace("<ip>", server.connect).replace("<map>", server.map).replace("<players>", server.players.length).replace("<maxplayers>", server.maxplayers)
+            if(server != null){
+                players += server.players.length;
+                maxplayers += server.maxplayers;
+                embed.addFields(
+                    {
+                        name: config.EmbedInfo.ServerField1.replace("<servername>", server.name), value: config.EmbedInfo.ServerField2.replace("<ip>", server.connect).replace("<map>", server.map).replace("<players>", server.players.length).replace("<maxplayers>", server.maxplayers)
+                    }
+                )
+                if(config.ShowPlayers){
+                    for(var o = 0; o < server.players.length; o++){
+                        var player = server.players[o];
+                        embed.addFields({
+                            name: ":bust_in_silhouette: " + player.name, value: ":hourglass_flowing_sand: " + GetTime(player.time), inline: true
+                        })
+                    }
                 }
-            )
+            }
         }
     }
     finally{
@@ -90,4 +108,13 @@ async function UpdateEmbed (message){
         embed.setDescription(config.EmbedInfo.Description.replace("<players>", players).replace("<maxplayers>", maxplayers));
         message.edit(embed);
     }
+}
+
+function GetTime(ms) {
+    ms *= 1000;
+    const sec = Math.floor((ms / 1000) % 60).toString()
+    const min = Math.floor((ms / (1000 * 60)) % 60).toString()
+    const hrs = Math.floor((ms / (1000 * 60 * 60)) % 60).toString()
+    const days = Math.floor((ms / (1000 * 60 * 60 * 24)) % 60).toString()
+    return `${days.padStart(1, `0`)}d ${hrs.padStart(2, `0`)}h ${min.padStart(2, `0`)}m ${sec.padStart(3, `0`)}s` 
 }
